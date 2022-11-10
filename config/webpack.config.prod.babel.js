a47d806fa3db459d3fb5ad8cf11f4c2954c5047f
nv <naverstay.me@gmail.com>
2022-11-10 17:21:59 +0100

    build

--- config/webpack.config.prod.babel.js
+++ config/webpack.config.prod.babel.js
@@ -2,7 +2,7 @@
 import webpack from 'webpack';
 
 // Import webpack plugins
-import {merge as webpackMerge} from 'webpack-merge';
+import { merge as webpackMerge } from 'webpack-merge';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
 import TerserPlugin from 'terser-webpack-plugin';
@@ -22,11 +22,11 @@ export default webpackMerge(baseConfig, {
         test: /\.(sa|sc|c)ss$/,
         use: [
           MiniCssExtractPlugin.loader,
-          {loader: 'css-loader', options: {sourceMap: true}},
-          {loader: 'sass-loader', options: {sourceMap: true}}
-        ]
-      }
-    ]
+          { loader: 'css-loader', options: { sourceMap: true } },
+          { loader: 'sass-loader', options: { sourceMap: true } },
+        ],
+      },
+    ],
   },
 
   plugins: [
@@ -34,60 +34,60 @@ export default webpackMerge(baseConfig, {
     // Note: style-loader is for development, MiniCssExtractPlugin is for production
     new MiniCssExtractPlugin({
       filename: '[name].[contenthash].css',
-      chunkFilename: '[id].[contenthash].css'
+      chunkFilename: '[id].[contenthash].css',
     }),
     new webpack.SourceMapDevToolPlugin({
       filename: '[file].map',
-      exclude: ['npm']
-    })
+      exclude: ['npm'],
+    }),
   ],
 
   optimization: {
     minimize: true,
     minimizer: [
       new CssMinimizerPlugin({
-        parallel: true
+        parallel: true,
       }),
       new TerserPlugin({
         parallel: true,
         extractComments: false,
         terserOptions: {
           output: {
-            comments: false
-          }
-        }
-      })
+            comments: false,
+          },
+        },
+      }),
     ],
 
     splitChunks: {
       chunks: 'all',
       maxInitialRequests: Infinity,
       minSize: 0,
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: (module) => {
-            // get the name. E.g. node_modules/packageName/not/this/part.js
-            // or node_modules/packageName
-            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-            // npm package names are URL-safe, but some servers don't like @ symbols
-            return `npm.${packageName.replace('@', '')}`;
-          }
-        }
-      }
+      //cacheGroups: {
+      //  vendor: {
+      //    test: /[\\/]node_modules[\\/]/,
+      //    name: (module) => {
+      //      // get the name. E.g. node_modules/packageName/not/this/part.js
+      //      // or node_modules/packageName
+      //      const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+      //      // npm package names are URL-safe, but some servers don't like @ symbols
+      //      return `npm.${packageName.replace('@', '')}`;
+      //    }
+      //  }
+      //}
     },
 
     // Once your build outputs multiple chunks, this option will ensure they share the webpack runtime
     // instead of having their own. This also helps with long-term caching, since the chunks will only
     // change when actual code changes, not the webpack runtime.
     runtimeChunk: {
-      name: 'runtime'
-    }
+      name: 'runtime',
+    },
   },
 
   performance: {
     hints: false,
     maxEntrypointSize: 512000,
-    maxAssetSize: 512000
-  }
+    maxAssetSize: 512000,
+  },
 });
